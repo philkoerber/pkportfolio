907939daf134f2ebfedf2855ad45e84512d75731
philkoerber <philipp.koerber.93@gmail.com>
2023-06-08 15:01:40 +0900

    some content updates

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -6,7 +6,7 @@ import ParallaxDiv from './ParallaxDiv';
 
 function CV(props) {
   return (
-    <div className='relative p-8'>
+    <div className='relative p-8 mb-5'>
       <div className='text-center text-4xl font-instrument m-24'>
         RÉSUMÉ
         <p className='font-urbanist text-xl'>...an overview where I have worked professionally</p>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,8 @@ import { motion } from 'framer-motion';
 
 import { GoMarkGithub, GoMail } from 'react-icons/go';
 import { HiOutlinePhone } from 'react-icons/hi'
-import {ImLocation} from "react-icons/im"
+import { ImLocation } from "react-icons/im"
+import {IoLogoLinkedin} from "react-icons/io"
 
 import line from "./pics/line.png"
 import pk from "./pics/pk.svg"
@@ -24,6 +25,10 @@ function Contact(props) {
     window.open("https://github.com/philkoerber", "_blank");
     };
     
+    const handleLinkedInClick = () => {
+    window.open("https://www.linkedin.com/in/philipp-koerber-p1993k1312/", "_blank");
+    };
+
     return (
         <div className='relative h-screen w-full flex justify-center items-center'>
             <div className='flex flex-col gap-10 items-center font-urbanist'>
@@ -38,7 +43,8 @@ function Contact(props) {
                     
                 </div>
                 <div className={styles} style={{cursor: "default"}}><ImLocation className='text-3xl'/>Kawaguchi, Saitama</div>
-                <motion.div className={styles} {...motionAnimation} onClick={handleMailClick}><GoMail className='text-3xl'/>philipp.koerber.93@gmail.com</motion.div>
+                <motion.div className={styles} {...motionAnimation} onClick={handleMailClick}><GoMail className='text-3xl' />philipp.koerber.93@gmail.com</motion.div>
+                <motion.div className={styles} {...motionAnimation} onClick={handleLinkedInClick}><IoLogoLinkedin className='text-[35px]'/>.../philipp-koerber-p1993k1312</motion.div>
                 <motion.div className={styles} {...motionAnimation} onClick={handleGithubClick}><GoMarkGithub className='text-3xl' />@philkoerber</motion.div>
                 <div className={styles} style={{cursor: "default"}}><HiOutlinePhone className='text-3xl'/>080 5625 4648</div>
                 <div className="flex flex-col justify-center items-center text-3xl font-semibold"><img className="w-[40%]" src={line} /></div>
