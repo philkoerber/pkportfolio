6e8e6eb0d93dfd0a6126f744f7d17c4a31cfc112
philkoerber <philipp.koerber.93@gmail.com>
2023-09-18 18:19:16 +0900

    add dza website to projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,87 +1,94 @@
-import React from 'react';
-import {motion} from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 
-import { GoMarkGithub, GoLink } from 'react-icons/go';
+import { GoMarkGithub, GoLink } from "react-icons/go";
 
-import ParallaxDiv from './ParallaxDiv';
+import ParallaxDiv from "./ParallaxDiv";
 
 const buttonStyles = {
   className:
-    'flex items-center bg-eyeblue text-isabelline font-semibold py-0 px-2 rounded-full cursor-pointer shadow-lg',
+    "flex items-center bg-eyeblue text-isabelline font-semibold py-0 px-2 rounded-full cursor-pointer shadow-lg",
 };
 
 const buttonAnimation = {
   whileHover: { scale: 1.1 },
-  whileTap: {scale: 0.9}
-}
+  whileTap: { scale: 0.9 },
+};
 
 function Project(props) {
   const content = props.content;
   return (
-    <ParallaxDiv className="relative w-fit max-w-[300px] h-fit font-urbanist"
-    offset={100}>
-
+    <ParallaxDiv
+      className="relative w-fit max-w-[300px] h-fit font-urbanist"
+      offset={100}>
       {/* PICTURE AND TITLE */}
 
       <div className="relative mx-auto w-[90%] md:w-full max-w-[300px]">
         <video
-          className='rounded-full shadow-xl'
-          src={content.file} alt="picture"
+          className="rounded-full shadow-xl"
+          src={content.file}
+          alt="picture"
           type="video/mp4"
           autoPlay
           playsInline
           loop
-          muted
-        >
+          muted>
           <source src={content.file} type="video/mp4" />
         </video>
-        
-        <svg className='absolute top-[-52%] rotate-[89deg]'
+
+        <svg
+          className="absolute top-[-52%] rotate-[89deg]"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 -100 200 300"
-          fill='none'>
-      <text textAnchor="left" className='text-center font-instrument text-2xl tracking-widest' x="50%" y="0%" dy="-0em" fill='#000000'>
-        <textPath xlinkHref="#text-curve">
-          {content.name}
-        </textPath>
-      </text>
-      <path id="text-curve" d="M150,150a100,100 0 1,1 0,-200a100,100 0 1,1 0,200" />
+          fill="none">
+          <text
+            textAnchor="left"
+            className="text-center font-instrument text-2xl tracking-widest"
+            x="50%"
+            y="0%"
+            dy="-0em"
+            fill="#000000">
+            <textPath xlinkHref="#text-curve">{content.name}</textPath>
+          </text>
+          <path
+            id="text-curve"
+            d="M150,150a100,100 0 1,1 0,-200a100,100 0 1,1 0,200"
+          />
         </svg>
       </div>
 
-      
-        <div className='relative mx-auto w-[80%] z-50 bg-opacity-95 p-3'>
-        <div className='mb-2'>
-          <p className='font-bold text-xl'>{content.content1}</p>
-          <p className=''>{content.content2}</p></div>
-        <div className='flex flex-wrap gap-x-2 gap-y-0 justify-end'>
+      <div className="relative mx-auto w-[80%] z-50 bg-opacity-95 p-3">
+        <div className="mb-2">
+          <p className="font-bold text-xl">{content.content1}</p>
+          <p className="">{content.content2}</p>
+        </div>
+        <div className="flex flex-wrap gap-x-2 gap-y-0 justify-end">
           {content.techs.map((tech, i) => {
             return (
-              <div key={i}  className='text-delftblue w-fit'>#{tech}</div>)
+              <div key={i} className="text-delftblue w-fit">
+                #{tech}
+              </div>
+            );
           })}
         </div>
       </div>
 
-      
       {/* BUTTONS */}
 
-      <div className='relative flex gap-2 justify-center text-xl z-100'>
-  <a href={content.github} target="_blank" rel="noopener noreferrer">
-    <motion.div {...buttonStyles} {...buttonAnimation}>
-      <GoMarkGithub className='text-3xl m-2'/>
-      GitHub
-    </motion.div>
-  </a>
-  <a href={content.home} target="_blank" rel="noopener noreferrer">
-    <motion.div {...buttonStyles} {...buttonAnimation}>
-      <GoLink className='text-3xl m-2'/>
-      Home
-    </motion.div>
-  </a>
-</div>
-      
-
-      
+      <div className="relative flex gap-2 justify-center text-xl z-100">
+        <a href={content.github} target="_blank" rel="noopener noreferrer">
+          <motion.div {...buttonStyles} {...buttonAnimation}>
+            <GoMarkGithub className="text-3xl m-2" />
+            GitHub
+          </motion.div>
+        </a>
+        <a href={content.home} target="_blank" rel="noopener noreferrer">
+          <motion.div {...buttonStyles} {...buttonAnimation}>
+            <GoLink className="text-3xl m-2" />
+            Home
+          </motion.div>
+        </a>
+      </div>
     </ParallaxDiv>
   );
 }
--- src/components/projectsArr.js
+++ src/components/projectsArr.js
@@ -1,58 +1,91 @@
 import sixtysix from "./pics/sixtysix.mp4";
-import openingexplorer from "./pics/openingexplorer.mp4"
-import portfolio from "./pics/portfolio.mp4"
-import puredata from "./pics/puredata.mp4"
-import docrob from "./pics/docrob.mp4"
-
+import openingexplorer from "./pics/openingexplorer.mp4";
+import portfolio from "./pics/portfolio.mp4";
+import puredata from "./pics/puredata.mp4";
+import docrob from "./pics/docrob.mp4";
+import dza from "./pics/dza.mp4";
+import lyraverse from "./pics/lyraverse.svg";
 
 const projectsArr = [
-    {
-        name: "DOC.ROB",
-        content1: "A chatbot that knows the React docs.",
-        content2: "Utilizing FlowiseAI, this app connects to a custom LLM that is trained on the current documentation of ReactJS.",
-        github: "https://github.com/philkoerber/docrob",
-        home: "https://docrob.vercel.app/",
-        techs: ["nextJS", "FlowiseAI", "tailwind", "zustand", ],
-        file: docrob
-    },
-    {
-        name: "Sixty-Six",
-        content1: "A web app for playing the german card game 'Sechsundsechzig'.",
-        content2: "In addition to handling game logic, this project provided me with an opportunity to explore the realm of framer-motion, sparking my passion for its possibilities.",
-        github: "https://github.com/philkoerber/sixtysix",
-        home: "https://sixty-six.netlify.app/",
-        techs: ["react", "framer-motion", "Chakra UI", "zustand"],
-        file: sixtysix
-    },
-    {
-        name: "Opening Explorer",
-        content1: "A chess opening explorer, animating the opening lines in a diagramm.",
-        content2: "As a fan of the world of chess openings, I found it fascinating to combine various chess-related libraries with an opening database, showcasing the integration through smooth animations. ",
-        github: "https://github.com/philkoerber/Opening-Name-Explorer",
-        home: "https://openingnameexplorer.netlify.app/",
-        techs: ["react", "chess.js", "chessboard.js", "framer-motion", "tailwind", "zustand"],
-        file: openingexplorer
-    },
-    {
-        name: "Portfolio",
-        content1: "The beautiful portfolio you are checking out now!",
-        content2: "This portfolio is created to present myself and showcase my skills to others.",
-        github: "https://github.com/philkoerber/pkportfolio",
-        home: "/",
-        techs: ["react", "framer-motion", "tailwind"],
-        file: portfolio
-    },
-    {
-        name: "Music",
-        content1: "Most of my music is made by utilizing Pure Data, a visual programming language.",
-        content2: "With 15 years of experience, Pure Data has been my programming foundation, enabling me to exercise complete authority over sound and one-of-a-kind audio designs.",
-        github: "https://github.com/philkoerber/puredata-objects",
-        home: "https://www.youtube.com/@swim8008",
-        techs: ["pure-data", "reaper"],
-        file: puredata
-
-    },
-    
-]
+  //   {
+  //     name: "LyraVerseAI",
+  //     content1: "A web app that lets you create, store and edit Lyrics and Poems",
+  //     content2:
+  //       "Utilizing different AI-APIs, this app provides interactive and fun ways to create some lyrics. Let the AI create your next line, or lean back and listen to your peotry. Everything is stored in a database, so access it from anywhere.",
+  //     github: "https://github.com/philkoerber/LyraVerseAI",
+  //     home: "https://lyraverse-ai.vercel.app/",
+  //     techs: ["nextJS", "FlowiseAI", "tailwind", "supabase"],
+  //     file: lyraverse,
+  //   },
+  {
+    name: "DOC.ROB",
+    content1: "A chatbot that knows the React docs.",
+    content2:
+      "Utilizing FlowiseAI, this app connects to a custom LLM that is trained on the current documentation of ReactJS.",
+    github: "https://github.com/philkoerber/docrob",
+    home: "https://docrob.vercel.app/",
+    techs: ["nextJS", "FlowiseAI", "tailwind", "zustand"],
+    file: docrob,
+  },
+  {
+    name: "Sixty-Six",
+    content1: "A web app for playing the german card game 'Sechsundsechzig'.",
+    content2:
+      "In addition to handling game logic, this project provided me with an opportunity to explore the realm of framer-motion, sparking my passion for its possibilities.",
+    github: "https://github.com/philkoerber/sixtysix",
+    home: "https://sixty-six.netlify.app/",
+    techs: ["react", "framer-motion", "Chakra UI", "zustand"],
+    file: sixtysix,
+  },
+  {
+    name: "DZA",
+    content1: "Website for Hip-Hop Artist DZA",
+    content2:
+      "Simple is best, so let the work of this talented artist speak for itself. 3D-Enviroment to underline his aesthetics.",
+    github: "https://github.com/philkoerber/dza",
+    home: "https://dza-ten.vercel.app/",
+    techs: [, "nextJS", "react", "react-force-graph", "tailwind"],
+    file: dza,
+  },
+  {
+    name: "Opening Explorer",
+    content1:
+      "A chess opening explorer, animating the opening lines in a diagramm.",
+    content2:
+      "As a fan of the world of chess openings, I found it fascinating to combine various chess-related libraries with an opening database, showcasing the integration through smooth animations. ",
+    github: "https://github.com/philkoerber/Opening-Name-Explorer",
+    home: "https://openingnameexplorer.netlify.app/",
+    techs: [
+      "react",
+      "chess.js",
+      "chessboard.js",
+      "framer-motion",
+      "tailwind",
+      "zustand",
+    ],
+    file: openingexplorer,
+  },
+  {
+    name: "Portfolio",
+    content1: "The beautiful portfolio you are checking out now!",
+    content2:
+      "This portfolio is created to present myself and showcase my skills to others.",
+    github: "https://github.com/philkoerber/pkportfolio",
+    home: "/",
+    techs: ["react", "framer-motion", "tailwind"],
+    file: portfolio,
+  },
+  {
+    name: "Music",
+    content1:
+      "Most of my music is made by utilizing Pure Data, a visual programming language.",
+    content2:
+      "With 15 years of experience, Pure Data has been my programming foundation, enabling me to exercise complete authority over sound and one-of-a-kind audio designs.",
+    github: "https://github.com/philkoerber/puredata-objects",
+    home: "https://www.youtube.com/@swim8008",
+    techs: ["pure-data", "reaper"],
+    file: puredata,
+  },
+];
 
 export default projectsArr;
